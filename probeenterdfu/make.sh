ba78cf04bb0191dc124bbcf9a9a81f8d62577da1
john <NyanSatan@users.noreply.github.com>
2025-09-11 17:35:56 +0200

    Compile probeenterdfu for both Intel & Apple Silicon

--- probeenterdfu/make.sh
+++ probeenterdfu/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clang -lc++ -framework IOKit -framework CoreFoundation -lAstrisAPI probeenterdfu.c -o probeenterdfu
+clang -arch x86_64 -arch arm64 -mmacosx-version-min=10.7 -lc++ -framework IOKit -framework CoreFoundation -lAstrisAPI probeenterdfu.c -o probeenterdfu
